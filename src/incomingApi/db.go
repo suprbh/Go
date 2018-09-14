3ffcaf83f446f5202c4f632e9d15edb0099087bb
Supriya Bhat <sups_bhat@yahoo.com>
2018-09-14 10:17:44 -0700

    Expanding the data and device information. Working on writing to database

--- src/incomingApi/db.go
+++ src/incomingApi/db.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
 )
 
 type connections struct {
 	dbUser   map[string]string
 	dbDevice map[string]string
-	// cass *cassandra.cctx  // TODO
+	// TODO
+	// cass *cassandra.cctx
 	// mydb *mysqldb.mctx
+	// red *redis.ctx
 }
 
 // DB : Temporarily using a map instead of actual Redis/Cassandra/MySql/MongoDB database
@@ -25,19 +29,26 @@ var dbDeviceCreds = map[string]string{
 	"apiKey2": " device2",
 }
 
-type deviceProfile struct {
+// DeviceProfile has all the information regarding the specific device
+type DeviceProfile struct {
 	name        string
 	deviceID    string
+	deviceType  string
 	modelNumber string
 	lat         string
 	lon         string
 }
 
-type userProfile struct {
+// UserProfile contains profile information of the user of the product
+type UserProfile struct {
 	name        string
 	address     string
 	memberSince time.Time
-	devices     []deviceProfile
+	devices     *[]DeviceProfile
+}
+
+// DeviceData contains the values of individual data points sent by the device.
+type DeviceData struct {
 }
 
 // putDevice updates the information about the device TODO
@@ -46,6 +57,26 @@ func (c *connections) putDevice(g *gin.Context) {
 }
 
 func (c *connections) putData(g *gin.Context) {
+
+	deviceID := g.Param("deviceID")
+
+	// Unmarshal and validate the payload.
+	var dd DeviceData
+	err := g.Bind(&dd)
+	if err != nil {
+		g.IndentedJSON(http.StatusBadRequest,
+			ErrorResponse{
+				ErrorType:    "PayloadDecodeError",
+				ErrorMessage: err.Error()})
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"Data":    dd,
+		"Station": deviceID,
+	}).Debug("Put Device data")
+
+	// TODO : add this data to the database
 
 }
 
